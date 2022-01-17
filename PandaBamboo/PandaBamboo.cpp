bd9e64c8a7c0f33bafca5b15774354c681f06c98
Jules <valet@LAPTOP-UD22KUIK>
2022-01-17 12:12:07 +0100

    Menu final

--- PandaBamboo/PandaBamboo.cpp
+++ PandaBamboo/PandaBamboo.cpp
@@ -3,10 +3,9 @@
 #include <SDL_ttf.h>
 #include "config_sdl.h"
 using namespace std;
-const int LARGEUR = 1000; //largeur fenetre
-const int HAUTEUR = 700;  //hauteur fenetre
+const int LARGEUR = 1000; 
+const int HAUTEUR = 500;  
 const int N = 6;
-const char image[] = "panda.png";
 SDL_Color blanc = {255,255,255};
 
 void menu_principal(SDL_Renderer* rendu,TTF_Font* font) {
@@ -15,12 +14,19 @@ void menu_principal(SDL_Renderer* rendu,TTF_Font* font) {
 	titre.y = 40;
 	titre.w = LARGEUR - titre.x*2;
 	titre.h = 100;
-	SDL_SetRenderDrawColor(rendu, 255, 255, 255,255);
-	SDL_RenderDrawRect(rendu, &titre);
+	
+	SDL_Rect titre_;
+	titre_.w = 450;
+	titre_.h = titre.h;
+	titre_.x = (titre.x + titre.w/2)-titre_.w/2;
+	titre_.y = 40;
+	
+	SDL_SetRenderDrawColor(rendu, 0, 0, 0, 255);
+	SDL_RenderDrawRect(rendu, &titre_);
 
 	SDL_Texture* texture1 = loadText(rendu, "Panda Garden", blanc, font);
-	SDL_QueryTexture(texture1, NULL, NULL, &titre.w, &titre.h);
-	SDL_RenderCopy(rendu, texture1, NULL, &titre);
+	SDL_QueryTexture(texture1, NULL, NULL, &titre_.w, &titre_.h);
+	SDL_RenderCopy(rendu, texture1, NULL, &titre_);
 	SDL_RenderPresent(rendu);
 	SDL_DestroyTexture(texture1);
 
@@ -29,16 +35,43 @@ void menu_principal(SDL_Renderer* rendu,TTF_Font* font) {
 	jeu_auto.h = 200;
 	jeu_auto.x = 80;
 	jeu_auto.y = HAUTEUR/2-jeu_auto.h/2;
-	SDL_RenderDrawRect(rendu, &jeu_auto);
-
+	
+	
 	SDL_Rect jeu_manuel;
 	jeu_manuel.w = jeu_auto.w;
 	jeu_manuel.h = 200;
 	jeu_manuel.x = LARGEUR/2+80;
 	jeu_manuel.y = HAUTEUR / 2 - jeu_manuel.h / 2;
-	SDL_RenderDrawRect(rendu, &jeu_manuel);
-	SDL_RenderDrawLine(rendu,LARGEUR/2,60+titre.h,LARGEUR/2,HAUTEUR);
-	SDL_RenderDrawLine(rendu, 0,HAUTEUR/2 ,LARGEUR, HAUTEUR/2);
+	
+
+
+	SDL_Rect jeu_auto_;
+	jeu_auto_.w = jeu_auto.w/2;
+	jeu_auto_.h = jeu_auto.h/2;
+	jeu_auto_.x = (jeu_auto.x + jeu_auto.w/2) -jeu_auto_.w/2;
+	jeu_auto_.y = jeu_auto.y + jeu_auto_.h/2;
+	
+
+	
+	SDL_Rect jeu_manuel_;
+	jeu_manuel_.w = jeu_manuel.w/2;
+	jeu_manuel_.h = jeu_manuel.h/2;
+	jeu_manuel_.x = (jeu_manuel.x + jeu_manuel.w / 2) - jeu_manuel_.w / 2;
+	jeu_manuel_.y = jeu_manuel.y + jeu_manuel_.h / 2;
+
+
+
+	SDL_Texture* texture2 = loadText(rendu, "Auto", blanc, font);
+	SDL_QueryTexture(texture2, NULL, NULL, &jeu_auto_.w, &jeu_auto_.h);
+	SDL_RenderCopy(rendu, texture2, NULL, &jeu_auto_);
+	SDL_RenderPresent(rendu);
+	SDL_DestroyTexture(texture2);
+
+	SDL_Texture* texture3 = loadText(rendu, "Manuel", blanc, font);
+	SDL_QueryTexture(texture3, NULL, NULL, &jeu_manuel_.w, &jeu_manuel_.h);
+	SDL_RenderCopy(rendu, texture3, NULL, &jeu_manuel_);
+	SDL_RenderPresent(rendu);
+	SDL_DestroyTexture(texture3);
 
 }
 
@@ -57,7 +90,7 @@ int main(int argn, char* argv[]) {
 	}
 
 
-	//on crée la fenêtre
+	
 	SDL_Window* win = SDL_CreateWindow("Menu Principal",
 		SDL_WINDOWPOS_CENTERED,     
 		SDL_WINDOWPOS_CENTERED,     
@@ -69,11 +102,11 @@ int main(int argn, char* argv[]) {
 	if (win == NULL)
 		cout << "erreur ouverture fenetre";
 
-	//Création d’un dessin associé à la fenêtre (1 seul renderer par fenetre)
+	
 	SDL_Renderer* rendu = SDL_CreateRenderer(
-		win,  //nom de la fenêtre
-		-1, //par défaut
-		SDL_RENDERER_ACCELERATED); //utilisation du GPU, valeur recommandée
+		win,  
+		-1, 
+		SDL_RENDERER_ACCELERATED); 
 	
 	TTF_Init();
 	TTF_Font* font = TTF_OpenFont("RobotoMono-Light.ttf", 60);
@@ -81,15 +114,15 @@ int main(int argn, char* argv[]) {
 	menu_principal(rendu,font);
 	SDL_RenderPresent(rendu);
 	
-	bool continuer = true;   //booléen fin de programme
-	SDL_Event event;//gestion des évènements souris/clavier, 
-					//SDL_Event est de type struct
+	bool continuer = true;   
+	SDL_Event event; 
+					
 	while (continuer)
 	{
-		SDL_WaitEvent(&event);//attente d’un évènement
-		switch (event.type) //test du type d’évènement
+		SDL_WaitEvent(&event);
+		switch (event.type) 
 		{
-		case SDL_QUIT: //clic sur la croix de fermeture
+		case SDL_QUIT: 
 					   
 			continuer = false;
 			break;
