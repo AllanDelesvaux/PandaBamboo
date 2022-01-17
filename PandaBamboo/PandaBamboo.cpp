69e8383304cef1ea46a8fec08acbe9a55a234f07
Jules <valet@LAPTOP-UD22KUIK>
2022-01-17 12:12:07 +0100

    Menu final

--- PandaBamboo/PandaBamboo.cpp
+++ PandaBamboo/PandaBamboo.cpp
@@ -3,42 +3,74 @@
 #include <SDL_ttf.h>
 #include "config_sdl.h"
 using namespace std;
-const int LARGEUR = 1000; //largeur fenetre
-const int HAUTEUR = 700;  //hauteur fenetre
-const int N = 6;
-const char image[] = "panda.png";
-SDL_Color blanc = {255,255,255};
+const int LARGEUR_ = 1000;
+const int HAUTEUR_ = 500;
+SDL_Color blanc = { 255,255,255 };
 
-void menu_principal(SDL_Renderer* rendu,TTF_Font* font) {
+void menu_principal(SDL_Renderer* rendu, TTF_Font* font) {
 	SDL_Rect titre;
 	titre.x = 80;
 	titre.y = 40;
-	titre.w = LARGEUR - titre.x*2;
+	titre.w = LARGEUR_ - titre.x * 2;
 	titre.h = 100;
-	SDL_SetRenderDrawColor(rendu, 255, 255, 255,255);
-	SDL_RenderDrawRect(rendu, &titre);
+
+	SDL_Rect titre_;
+	titre_.w = 450;
+	titre_.h = titre.h;
+	titre_.x = (titre.x + titre.w / 2) - titre_.w / 2;
+	titre_.y = 40;
+
+	SDL_SetRenderDrawColor(rendu, 0, 0, 0, 255);
+	SDL_RenderDrawRect(rendu, &titre_);
 
 	SDL_Texture* texture1 = loadText(rendu, "Panda Garden", blanc, font);
-	SDL_QueryTexture(texture1, NULL, NULL, &titre.w, &titre.h);
-	SDL_RenderCopy(rendu, texture1, NULL, &titre);
+	SDL_QueryTexture(texture1, NULL, NULL, &titre_.w, &titre_.h);
+	SDL_RenderCopy(rendu, texture1, NULL, &titre_);
 	SDL_RenderPresent(rendu);
 	SDL_DestroyTexture(texture1);
 
 	SDL_Rect jeu_auto;
-	jeu_auto.w = LARGEUR/2-80*2;
+	jeu_auto.w = LARGEUR_ / 2 - 80 * 2;
 	jeu_auto.h = 200;
 	jeu_auto.x = 80;
-	jeu_auto.y = HAUTEUR/2-jeu_auto.h/2;
-	SDL_RenderDrawRect(rendu, &jeu_auto);
+	jeu_auto.y = HAUTEUR_ / 2 - jeu_auto.h / 2;
+
 
 	SDL_Rect jeu_manuel;
 	jeu_manuel.w = jeu_auto.w;
 	jeu_manuel.h = 200;
-	jeu_manuel.x = LARGEUR/2+80;
-	jeu_manuel.y = HAUTEUR / 2 - jeu_manuel.h / 2;
-	SDL_RenderDrawRect(rendu, &jeu_manuel);
-	SDL_RenderDrawLine(rendu,LARGEUR/2,60+titre.h,LARGEUR/2,HAUTEUR);
-	SDL_RenderDrawLine(rendu, 0,HAUTEUR/2 ,LARGEUR, HAUTEUR/2);
+	jeu_manuel.x = LARGEUR_ / 2 + 80;
+	jeu_manuel.y = HAUTEUR_ / 2 - jeu_manuel.h / 2;
+
+
+
+	SDL_Rect jeu_auto_;
+	jeu_auto_.w = jeu_auto.w / 2;
+	jeu_auto_.h = jeu_auto.h / 2;
+	jeu_auto_.x = (jeu_auto.x + jeu_auto.w / 2) - jeu_auto_.w / 2;
+	jeu_auto_.y = jeu_auto.y + jeu_auto_.h / 2;
+
+
+
+	SDL_Rect jeu_manuel_;
+	jeu_manuel_.w = jeu_manuel.w / 2;
+	jeu_manuel_.h = jeu_manuel.h / 2;
+	jeu_manuel_.x = (jeu_manuel.x + jeu_manuel.w / 2) - jeu_manuel_.w / 2;
+	jeu_manuel_.y = jeu_manuel.y + jeu_manuel_.h / 2;
+
+
+
+	SDL_Texture* texture2 = loadText(rendu, "Auto", blanc, font);
+	SDL_QueryTexture(texture2, NULL, NULL, &jeu_auto_.w, &jeu_auto_.h);
+	SDL_RenderCopy(rendu, texture2, NULL, &jeu_auto_);
+	SDL_RenderPresent(rendu);
+	SDL_DestroyTexture(texture2);
+
+	SDL_Texture* texture3 = loadText(rendu, "Manuel", blanc, font);
+	SDL_QueryTexture(texture3, NULL, NULL, &jeu_manuel_.w, &jeu_manuel_.h);
+	SDL_RenderCopy(rendu, texture3, NULL, &jeu_manuel_);
+	SDL_RenderPresent(rendu);
+	SDL_DestroyTexture(texture3);
 
 }
 
@@ -57,50 +89,50 @@ int main(int argn, char* argv[]) {
 	}
 
 
-	//on crée la fenêtre
+
 	SDL_Window* win = SDL_CreateWindow("Menu Principal",
-		SDL_WINDOWPOS_CENTERED,     
-		SDL_WINDOWPOS_CENTERED,     
-		LARGEUR, 					
-		HAUTEUR, 			
-		SDL_WINDOW_SHOWN 
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		LARGEUR_,
+		HAUTEUR_,
+		SDL_WINDOW_SHOWN
 	);
-	
+
 	if (win == NULL)
 		cout << "erreur ouverture fenetre";
 
-	//Création d’un dessin associé à la fenêtre (1 seul renderer par fenetre)
+
 	SDL_Renderer* rendu = SDL_CreateRenderer(
-		win,  //nom de la fenêtre
-		-1, //par défaut
-		SDL_RENDERER_ACCELERATED); //utilisation du GPU, valeur recommandée
-	
+		win,
+		-1,
+		SDL_RENDERER_ACCELERATED);
+
 	TTF_Init();
 	TTF_Font* font = TTF_OpenFont("RobotoMono-Light.ttf", 60);
 	TTF_Quit();
-	menu_principal(rendu,font);
+	menu_principal(rendu, font);
 	SDL_RenderPresent(rendu);
-	
-	bool continuer = true;   //booléen fin de programme
-	SDL_Event event;//gestion des évènements souris/clavier, 
-					//SDL_Event est de type struct
+
+	bool continuer = true;
+	SDL_Event event;
+
 	while (continuer)
 	{
-		SDL_WaitEvent(&event);//attente d’un évènement
-		switch (event.type) //test du type d’évènement
+		SDL_WaitEvent(&event);
+		switch (event.type)
 		{
-		case SDL_QUIT: //clic sur la croix de fermeture
-					   
+		case SDL_QUIT:
+
 			continuer = false;
 			break;
 
 		case SDL_MOUSEBUTTONUP:
 			if (event.button.button == SDL_BUTTON_LEFT && in_menu) {
-				if (event.button.x > 80 && event.button.x < LARGEUR/2 - 80 && event.button.y>HAUTEUR / 2 - 100 && event.button.y < HAUTEUR/2+100) {
+				if (event.button.x > 80 && event.button.x < LARGEUR_ / 2 - 80 && event.button.y>HAUTEUR_ / 2 - 100 && event.button.y < HAUTEUR_ / 2 + 100) {
 					cout << "ça marche\n";
 					in_menu = false;
 				}
-				else if (event.button.x > LARGEUR/2+80 && event.button.x < LARGEUR  - 80 && event.button.y>HAUTEUR / 2 - 100 && event.button.y < HAUTEUR / 2 + 100) {
+				else if (event.button.x > LARGEUR_ / 2 + 80 && event.button.x < LARGEUR_ - 80 && event.button.y>HAUTEUR_ / 2 - 100 && event.button.y < HAUTEUR_ / 2 + 100) {
 					cout << "ça marche\n";
 					in_menu = false;
 				}
@@ -108,9 +140,9 @@ int main(int argn, char* argv[]) {
 		}
 
 	}
-	
+
 	SDL_DestroyRenderer(rendu);
-	SDL_DestroyWindow(win);  
+	SDL_DestroyWindow(win);
 	SDL_Quit();
 	return 0;
 }
