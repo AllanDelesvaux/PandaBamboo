febd622612754764e288bbe24c0397fc7c250fa3
Allan Delesvaux <delesvaux.allan@gmail.com>
2022-01-17 15:01:37 +0100

    SDL Final manuel

--- PandaBamboo/PandaBamboo.cpp
+++ PandaBamboo/PandaBamboo.cpp
@@ -22,52 +22,44 @@ void DevMax(int ) {
 
 void interface_auto(SDL_Renderer* rendu) {
 
-	SDL_Rect Robot;
-	Robot.x = 120;
-	Robot.y = 740;
-	Robot.w = 60;
-	Robot.h = 80;
-	SDL_SetRenderDrawColor(rendu, 0, 255, 255, 255);
-	SDL_RenderFillRect(rendu, &Robot);
-
-	for (int i = 20; i < 820; i+=160) {
-		for (int j = 20; j < 820; j += 160) {
+	for (int i = 20; i < 820; i+=200) {
+		for (int j = 20; j < 820; j += 200) {
 			SDL_Rect bambooPlace;
 			bambooPlace.x = j;
 			bambooPlace.y = i;
-			bambooPlace.w = 160;
-			bambooPlace.h = 160;
+			bambooPlace.w = 200;
+			bambooPlace.h = 200;
 			SDL_SetRenderDrawColor(rendu, 255, 255, 255, 255);
 			SDL_RenderDrawRect(rendu, &bambooPlace);
 
 			SDL_Rect MaxHauteur;
 			MaxHauteur.x = j+1;
 			MaxHauteur.y = i+1;
-			MaxHauteur.w = 158;
-			MaxHauteur.h = 19;
+			MaxHauteur.w = 198;
+			MaxHauteur.h = 24;
 			SDL_SetRenderDrawColor(rendu, 0, 0, 255, 255);
 			SDL_RenderFillRect(rendu, &MaxHauteur);
 
 			SDL_Rect bambooSeparation;
-			bambooSeparation.x = j+100;
-			bambooSeparation.y = i+80;
-			bambooSeparation.w = 60;
-			bambooSeparation.h = 80;
+			bambooSeparation.x = j+125;
+			bambooSeparation.y = i+100;
+			bambooSeparation.w = 75;
+			bambooSeparation.h = 100;
 			SDL_SetRenderDrawColor(rendu, 255, 255, 255, 255);
 			SDL_RenderDrawRect(rendu, &bambooSeparation);
 
 			SDL_Rect bamboo;
-			bambooSeparation.x = j+50;
-			bambooSeparation.y = i+40;
-			bambooSeparation.w = 10;
-			bambooSeparation.h = 120;
+			bamboo.x = j+63;
+			bamboo.y = i+60;
+			bamboo.w = 10;
+			bamboo.h = 139;
 			SDL_SetRenderDrawColor(rendu, 0, 255, 0, 255);
-			SDL_RenderFillRect(rendu, &bambooSeparation);
+			SDL_RenderFillRect(rendu, &bamboo);
 
-			for (int k = i; k < i + 120; k += 30) {
+			for (int k = i; k < i + 139; k += 30) {
 				SDL_Rect bambooIncrease;
-				bambooIncrease.x = j+47;
-				bambooIncrease.y = k+40;
+				bambooIncrease.x = j+60;
+				bambooIncrease.y = k+60;
 				bambooIncrease.w = 16;
 				bambooIncrease.h = 3;
 				SDL_SetRenderDrawColor(rendu,0, 128, 0, 255);
@@ -77,95 +69,84 @@ void interface_auto(SDL_Renderer* rendu) {
 	}
 }
 
-void panda(SDL_Renderer* rendu) {
-	SDL_Surface img = IMG_Load(rendu, "");
+int panda(SDL_Renderer* rendu,int x, int y) {
+	SDL_Surface* image = IMG_Load("panda1.PNG");
+	if (!image)
+	{
+		printf("Erreur de chargement de l'image : %s", SDL_GetError());
+		return -1;
+	}
 
+	SDL_Texture* monImage = SDL_CreateTextureFromSurface(rendu, image);
+	SDL_FreeSurface(image);
+
+	SDL_Rect posImg;
+	posImg.x = x;
+	posImg.y = y;
+	
+	SDL_QueryTexture(monImage, NULL, NULL, &posImg.w, &posImg.h);
+	SDL_RenderCopy(rendu, monImage, NULL, &posImg);
+	SDL_RenderPresent(rendu);
 }
-
-
 
 
 void Left(SDL_Renderer* rendu,int &x, int &y) {
 	SDL_Rect RobotLft;
 	RobotLft.x = x+1;
 	RobotLft.y = y+1;
-	RobotLft.w = 58;
-	RobotLft.h = 78;
+	RobotLft.w = 73;
+	RobotLft.h = 98;
 	SDL_SetRenderDrawColor(rendu, 0, 0, 0, 255);
 	SDL_RenderFillRect(rendu, &RobotLft);
 
-	x -= 160;
+	x -= 200;
 
-	SDL_Rect RobotLft1;
-	RobotLft1.x = x+1;
-	RobotLft1.y = y+1;
-	RobotLft1.w = 58;
-	RobotLft1.h = 78;
-	SDL_SetRenderDrawColor(rendu, 255, 255, 255, 255);
-	SDL_RenderFillRect(rendu, &RobotLft1);
+	panda(rendu, x, y);
 }
 
 void Right(SDL_Renderer* rendu,int &x, int &y) {
 	SDL_Rect RobotRght;
 	RobotRght.x = x+1;
 	RobotRght.y = y+1;
-	RobotRght.w = 58;
-	RobotRght.h = 78;
+	RobotRght.w = 73;
+	RobotRght.h = 98;
 	SDL_SetRenderDrawColor(rendu, 0, 0, 0, 255);
 	SDL_RenderFillRect(rendu, &RobotRght);
 
-	x += 160;
+	x += 200;
 
-	SDL_Rect RobotRght1;
-	RobotRght1.x = x+1;
-	RobotRght1.y = y+1;
-	RobotRght1.w = 58;
-	RobotRght1.h = 78;
-	SDL_SetRenderDrawColor(rendu, 255, 255, 255, 255);
-	SDL_RenderFillRect(rendu, &RobotRght1);
+	panda(rendu, x, y);
 }
 void Up(SDL_Renderer* rendu,int &x, int &y) {
 	SDL_Rect RobotUp;
 	RobotUp.x = x+1;
 	RobotUp.y = y+1;
-	RobotUp.w = 58;
-	RobotUp.h = 78;
+	RobotUp.w = 73;
+	RobotUp.h = 98;
 	SDL_SetRenderDrawColor(rendu, 0, 0, 0, 255);
 	SDL_RenderFillRect(rendu, &RobotUp);
 
-	y -= 160;
+	y -= 200;
 
-	SDL_Rect RobotUp1;
-	RobotUp1.x = x+1;
-	RobotUp1.y = y+1;
-	RobotUp1.w = 58;
-	RobotUp1.h = 78;
-	SDL_SetRenderDrawColor(rendu, 255, 255, 255, 255);
-	SDL_RenderFillRect(rendu, &RobotUp1);
+	panda(rendu, x, y);
 }
 void Down(SDL_Renderer* rendu,int &x, int &y) {
 	SDL_Rect RobotDwn;
 	RobotDwn.x = x+1;
 	RobotDwn.y = y+1;
-	RobotDwn.w = 58;
-	RobotDwn.h = 78;
+	RobotDwn.w = 73;
+	RobotDwn.h = 98;
 	SDL_SetRenderDrawColor(rendu, 0, 0, 0, 255);
 	SDL_RenderFillRect(rendu, &RobotDwn);
 
-	y += 160;
+	y += 200;
 
-	SDL_Rect RobotDwn1;
-	RobotDwn1.x = x+1;
-	RobotDwn1.y = y+1;
-	RobotDwn1.w = 58;
-	RobotDwn1.h = 78;
-	SDL_SetRenderDrawColor(rendu, 255, 255, 255, 255);
-	SDL_RenderFillRect(rendu, &RobotDwn1);
+	panda(rendu, x, y);
 }
 
 int main(int argn, char* argv[]) {
-	int x = 120;
-	int y = 740;
+	int x = 145;
+	int y = 720;
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 		cout << "Echec à l’ouverture";
 		return 1;
@@ -187,6 +168,7 @@ int main(int argn, char* argv[]) {
 		SDL_RENDERER_ACCELERATED);
 	
 	interface_auto(rendu);
+	panda(rendu, x, y);
 	SDL_RenderPresent(rendu);
 
 	bool continuer = true; 
@@ -202,7 +184,7 @@ int main(int argn, char* argv[]) {
 			continuer = false;
 			break;
 		case SDL_KEYDOWN:
-			if (event.key.keysym.sym == SDLK_LEFT && x > 140) {
+			if (event.key.keysym.sym == SDLK_LEFT && x > 145) {
 				Left(rendu,x,y);
 				SDL_RenderPresent(rendu);
 			}
@@ -210,20 +192,20 @@ int main(int argn, char* argv[]) {
 				Right(rendu, x, y);
 				SDL_RenderPresent(rendu);
 			}
-			if (event.key.keysym.sym == SDLK_UP && y > 160) {
+			if (event.key.keysym.sym == SDLK_UP && y > 180) {
 				Up(rendu, x, y);
 				SDL_RenderPresent(rendu);
 			}
-			if (event.key.keysym.sym == SDLK_DOWN && y < 740) {
+			if (event.key.keysym.sym == SDLK_DOWN && y < 720) {
 				Down(rendu, x, y);
 				SDL_RenderPresent(rendu);
 			}
 			if (event.key.keysym.sym == SDLK_RETURN) {
 				SDL_Rect Cut;
-				Cut.x = x-53;
+				Cut.x = x-73;
 				Cut.y = y-60;
-				Cut.w = 20;
-				Cut.h = 134;
+				Cut.w = 40;
+				Cut.h = 154;
 				SDL_SetRenderDrawColor(rendu, 0, 0, 0, 255);
 				SDL_RenderFillRect(rendu, &Cut);
 				SDL_RenderPresent(rendu);
