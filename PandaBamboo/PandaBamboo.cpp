a707ecbf48fc420e04436a3d1c634396120f7d70
Allan Delesvaux <delesvaux.allan@gmail.com>
2022-01-17 15:57:43 +0100

    master merge erzkjgfzoerugnojezngoezrngbonezobnzsnbojezngo

--- PandaBamboo/PandaBamboo.cpp
+++ PandaBamboo/PandaBamboo.cpp
@@ -3,14 +3,17 @@
 #include <SDL_ttf.h>
 #include "config_sdl.h"
 using namespace std;
+const int LARGEUR_ = 1000;
+const int HAUTEUR_ = 500;
+SDL_Color blanc = { 255,255,255 };
 
-const int LARGEUR = 1200; 
-const int HAUTEUR = 900;  
+const int LARGEUR = 1200;
+const int HAUTEUR = 900;
 const int N = 6;
 
 void interface_auto(SDL_Renderer* rendu) {
 
-	for (int i = 20; i < 820; i+=200) {
+	for (int i = 20; i < 820; i += 200) {
 		for (int j = 20; j < 820; j += 200) {
 			SDL_Rect bambooPlace;
 			bambooPlace.x = j;
@@ -21,24 +24,24 @@ void interface_auto(SDL_Renderer* rendu) {
 			SDL_RenderDrawRect(rendu, &bambooPlace);
 
 			SDL_Rect MaxHauteur;
-			MaxHauteur.x = j+1;
-			MaxHauteur.y = i+1;
+			MaxHauteur.x = j + 1;
+			MaxHauteur.y = i + 1;
 			MaxHauteur.w = 198;
 			MaxHauteur.h = 24;
 			SDL_SetRenderDrawColor(rendu, 0, 0, 255, 255);
 			SDL_RenderFillRect(rendu, &MaxHauteur);
 
 			SDL_Rect bambooSeparation;
-			bambooSeparation.x = j+125;
-			bambooSeparation.y = i+100;
+			bambooSeparation.x = j + 125;
+			bambooSeparation.y = i + 100;
 			bambooSeparation.w = 75;
 			bambooSeparation.h = 100;
 			SDL_SetRenderDrawColor(rendu, 255, 255, 255, 255);
 			SDL_RenderDrawRect(rendu, &bambooSeparation);
 
 			SDL_Rect bamboo;
-			bamboo.x = j+63;
-			bamboo.y = i+60;
+			bamboo.x = j + 63;
+			bamboo.y = i + 60;
 			bamboo.w = 10;
 			bamboo.h = 139;
 			SDL_SetRenderDrawColor(rendu, 0, 255, 0, 255);
@@ -46,17 +49,18 @@ void interface_auto(SDL_Renderer* rendu) {
 
 			for (int k = i; k < i + 139; k += 30) {
 				SDL_Rect bambooIncrease;
-				bambooIncrease.x = j+60;
-				bambooIncrease.y = k+60;
+				bambooIncrease.x = j + 60;
+				bambooIncrease.y = k + 60;
 				bambooIncrease.w = 16;
 				bambooIncrease.h = 3;
-				SDL_SetRenderDrawColor(rendu,0, 128, 0, 255);
+				SDL_SetRenderDrawColor(rendu, 0, 128, 0, 255);
 				SDL_RenderFillRect(rendu, &bambooIncrease);
 			}
 		}
 	}
 }
-int panda(SDL_Renderer* rendu,int x, int y) {
+
+int panda(SDL_Renderer* rendu, int x, int y) {
 	SDL_Surface* image = IMG_Load("panda1.PNG");
 	if (!image)
 	{
@@ -70,15 +74,17 @@ int panda(SDL_Renderer* rendu,int x, int y) {
 	SDL_Rect posImg;
 	posImg.x = x;
 	posImg.y = y;
-	
+
 	SDL_QueryTexture(monImage, NULL, NULL, &posImg.w, &posImg.h);
 	SDL_RenderCopy(rendu, monImage, NULL, &posImg);
 	SDL_RenderPresent(rendu);
 }
-void Left(SDL_Renderer* rendu,int &x, int &y) {
+
+
+void Left(SDL_Renderer* rendu, int& x, int& y) {
 	SDL_Rect RobotLft;
-	RobotLft.x = x+1;
-	RobotLft.y = y+1;
+	RobotLft.x = x + 1;
+	RobotLft.y = y + 1;
 	RobotLft.w = 73;
 	RobotLft.h = 98;
 	SDL_SetRenderDrawColor(rendu, 0, 0, 0, 255);
@@ -88,10 +94,11 @@ void Left(SDL_Renderer* rendu,int &x, int &y) {
 
 	panda(rendu, x, y);
 }
-void Right(SDL_Renderer* rendu,int &x, int &y) {
+
+void Right(SDL_Renderer* rendu, int& x, int& y) {
 	SDL_Rect RobotRght;
-	RobotRght.x = x+1;
-	RobotRght.y = y+1;
+	RobotRght.x = x + 1;
+	RobotRght.y = y + 1;
 	RobotRght.w = 73;
 	RobotRght.h = 98;
 	SDL_SetRenderDrawColor(rendu, 0, 0, 0, 255);
@@ -101,10 +108,10 @@ void Right(SDL_Renderer* rendu,int &x, int &y) {
 
 	panda(rendu, x, y);
 }
-void Up(SDL_Renderer* rendu,int &x, int &y) {
+void Up(SDL_Renderer* rendu, int& x, int& y) {
 	SDL_Rect RobotUp;
-	RobotUp.x = x+1;
-	RobotUp.y = y+1;
+	RobotUp.x = x + 1;
+	RobotUp.y = y + 1;
 	RobotUp.w = 73;
 	RobotUp.h = 98;
 	SDL_SetRenderDrawColor(rendu, 0, 0, 0, 255);
@@ -114,10 +121,10 @@ void Up(SDL_Renderer* rendu,int &x, int &y) {
 
 	panda(rendu, x, y);
 }
-void Down(SDL_Renderer* rendu,int &x, int &y) {
+void Down(SDL_Renderer* rendu, int& x, int& y) {
 	SDL_Rect RobotDwn;
-	RobotDwn.x = x+1;
-	RobotDwn.y = y+1;
+	RobotDwn.x = x + 1;
+	RobotDwn.y = y + 1;
 	RobotDwn.w = 73;
 	RobotDwn.h = 98;
 	SDL_SetRenderDrawColor(rendu, 0, 0, 0, 255);
@@ -127,7 +134,8 @@ void Down(SDL_Renderer* rendu,int &x, int &y) {
 
 	panda(rendu, x, y);
 }
-int jeu(int argn, char* argv[]) {
+
+int jeu() {
 	int x = 145;
 	int y = 720;
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -198,7 +206,169 @@ int jeu(int argn, char* argv[]) {
 	SDL_DestroyRenderer(rendu);
 	SDL_DestroyWindow(win);
 	SDL_Quit();
-	
+	return 0;
+}
+
+struct Bamboo {
+	int taille;
+	int croissance;
+	int x;
+	int y;
+};
+
+int Reduce_Max(Bamboo* tab, int taille) {
+	int max = 0;
+	for (int i = 0; i < taille; i++) {
+		if (tab[i].taille > max) {
+			max = i;
+		}
+	}
+	tab[max].taille = 0;
+	return (max);
+}
+
+void menu_principal(SDL_Renderer* rendu, TTF_Font* font) {
+	SDL_Rect titre;
+	titre.x = 80;
+	titre.y = 40;
+	titre.w = LARGEUR_ - titre.x * 2;
+	titre.h = 100;
+
+	SDL_Rect titre_;
+	titre_.w = 450;
+	titre_.h = titre.h;
+	titre_.x = (titre.x + titre.w / 2) - titre_.w / 2;
+	titre_.y = 40;
+
+	SDL_SetRenderDrawColor(rendu, 0, 0, 0, 255);
+	SDL_RenderDrawRect(rendu, &titre_);
+
+	SDL_Texture* texture1 = loadText(rendu, "Panda Garden", blanc, font);
+	SDL_QueryTexture(texture1, NULL, NULL, &titre_.w, &titre_.h);
+	SDL_RenderCopy(rendu, texture1, NULL, &titre_);
+	SDL_RenderPresent(rendu);
+	SDL_DestroyTexture(texture1);
+
+	SDL_Rect jeu_auto;
+	jeu_auto.w = LARGEUR_ / 2 - 80 * 2;
+	jeu_auto.h = 200;
+	jeu_auto.x = 80;
+	jeu_auto.y = HAUTEUR_ / 2 - jeu_auto.h / 2;
+
+
+	SDL_Rect jeu_manuel;
+	jeu_manuel.w = jeu_auto.w;
+	jeu_manuel.h = 200;
+	jeu_manuel.x = LARGEUR_ / 2 + 80;
+	jeu_manuel.y = HAUTEUR_ / 2 - jeu_manuel.h / 2;
+
+
+
+	SDL_Rect jeu_auto_;
+	jeu_auto_.w = jeu_auto.w / 2;
+	jeu_auto_.h = jeu_auto.h / 2;
+	jeu_auto_.x = (jeu_auto.x + jeu_auto.w / 2) - jeu_auto_.w / 2;
+	jeu_auto_.y = jeu_auto.y + jeu_auto_.h / 2;
+
+
+
+	SDL_Rect jeu_manuel_;
+	jeu_manuel_.w = jeu_manuel.w / 2;
+	jeu_manuel_.h = jeu_manuel.h / 2;
+	jeu_manuel_.x = (jeu_manuel.x + jeu_manuel.w / 2) - jeu_manuel_.w / 2;
+	jeu_manuel_.y = jeu_manuel.y + jeu_manuel_.h / 2;
+
+
+
+	SDL_Texture* texture2 = loadText(rendu, "Auto", blanc, font);
+	SDL_QueryTexture(texture2, NULL, NULL, &jeu_auto_.w, &jeu_auto_.h);
+	SDL_RenderCopy(rendu, texture2, NULL, &jeu_auto_);
+	SDL_RenderPresent(rendu);
+	SDL_DestroyTexture(texture2);
+
+	SDL_Texture* texture3 = loadText(rendu, "Manuel", blanc, font);
+	SDL_QueryTexture(texture3, NULL, NULL, &jeu_manuel_.w, &jeu_manuel_.h);
+	SDL_RenderCopy(rendu, texture3, NULL, &jeu_manuel_);
+	SDL_RenderPresent(rendu);
+	SDL_DestroyTexture(texture3);
+
+}
+
+
+int main(int argn, char* argv[]) {
+	const int nbr_bamboo = 5;
+
+	Bamboo forest[nbr_bamboo];
+
+	bool in_menu = true;
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		cout << "Echec à l’ouverture";
+		return 1;
+	}
+	else if (TTF_Init() == -1)
+	{
+		printf("TTF_Init: %s\n", TTF_GetError());
+		exit(2);
+	}
+
+
+
+	SDL_Window* win = SDL_CreateWindow("Menu Principal",
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		LARGEUR_,
+		HAUTEUR_,
+		SDL_WINDOW_SHOWN
+	);
+
+	if (win == NULL)
+		cout << "erreur ouverture fenetre";
+
+
+	SDL_Renderer* rendu = SDL_CreateRenderer(
+		win,
+		-1,
+		SDL_RENDERER_ACCELERATED);
+
+	TTF_Init();
+	TTF_Font* font = TTF_OpenFont("RobotoMono-Light.ttf", 60);
+	TTF_Quit();
+	menu_principal(rendu, font);
+	SDL_RenderPresent(rendu);
+
+	bool continuer = true;
+	SDL_Event event;
+
+	while (continuer)
+	{
+		SDL_WaitEvent(&event);
+		switch (event.type)
+		{
+		case SDL_QUIT:
+
+			continuer = false;
+			break;
+
+		case SDL_MOUSEBUTTONUP:
+			if (event.button.button == SDL_BUTTON_LEFT && in_menu) {
+				if (event.button.x > 80 && event.button.x < LARGEUR_ / 2 - 80 && event.button.y>HAUTEUR_ / 2 - 100 && event.button.y < HAUTEUR_ / 2 + 100) {
+					cout << "ça marche\n";
+					in_menu = false;
+				}
+				else if (event.button.x > LARGEUR_ / 2 + 80 && event.button.x < LARGEUR_ - 80 && event.button.y>HAUTEUR_ / 2 - 100 && event.button.y < HAUTEUR_ / 2 + 100) {
+					cout << "ça marche\n";
+					in_menu = false;
+					SDL_DestroyRenderer(rendu);
+					SDL_DestroyWindow(win);
+					jeu();
+				}
+			}
+		}
+	}
+
+	SDL_DestroyRenderer(rendu);
+	SDL_DestroyWindow(win);
+	SDL_Quit();
 	return 0;
 }
 
